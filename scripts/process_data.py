0660aa6a5082424aeffcf4d2bb752de41868b073
Rebecca Barnes <rebeccaebarnes@gmail.com>
2019-02-26 09:50:10 -0800

    feat: Add process_data script

---
+++ scripts/process_data.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def load_data(messages_filepath, categories_filepath):
+    pass
+
+
+def clean_data(df):
+    pass
+
+
+def save_data(df, database_filename):
+    pass  
+
+
+def main():
+    print(sys.argv)
+    print(len(sys.argv))
+
+if __name__ == '__main__':
+    main()
